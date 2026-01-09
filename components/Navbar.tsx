014c8741491671214a944912690e334fb9f17bf4
ALVIN GEORGE <Alvingeorge_@outlook.com>
2026-01-09 16:27:22 +0530

    fixes #10

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,9 +17,9 @@ const Navbar: React.FC = () => {
         <div className="flex justify-between h-16 items-center">
           <div className="flex items-center gap-2">
             <div className="bg-teal-600 p-2 rounded-lg">
-              <Wallet className="h-6 w-6 text-white" />
+              <a href='http://localhost:5173/'> <Wallet className="h-6 w-6 text-white" /></a>
             </div>
-            <h1 className="text-xl font-bold text-white tracking-tight">SpendWise</h1>
+            <a href='http://localhost:5173/'><h1 className="text-xl font-bold text-white tracking-tight">SpendWise</h1></a>
           </div>
           <button
             onClick={handleLogout}
@@ -30,7 +30,7 @@ const Navbar: React.FC = () => {
           </button>
         </div>
       </div>
-    </nav>
+    </nav >
   );
 };
 
