ccd66b8caa912f441167042ad95c33772fc53cbf
ALVIN GEORGE <Alvingeorge_@outlook.com>
2026-01-09 16:19:31 +0530

    fixes #8

--- components/TransactionForm.tsx
+++ components/TransactionForm.tsx
@@ -15,7 +15,7 @@ const TransactionForm: React.FC<TransactionFormProps> = ({ onAddTransaction, isL
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     if (!title || amount) return;
-    
+
     await onAddTransaction(title, parseFloat(amount), type);
     setTitle('');
     setAmount('');
@@ -65,7 +65,7 @@ const TransactionForm: React.FC<TransactionFormProps> = ({ onAddTransaction, isL
                 <option value="income">Income</option>
               </select>
               <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-zinc-400">
-                <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
               </div>
             </div>
           </div>
